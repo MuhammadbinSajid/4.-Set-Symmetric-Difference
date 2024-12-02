1bba84339d73b4ec15f00d330fbbb6b5b4a67951
MuhammadbinSajid <mohammedsajidamazai@gmail.com>
2024-12-02 20:31:12 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Define two sets
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+# Find the symmetric difference
+symmetric_diff = set1.symmetric_difference(set2)
+
+# Print the sets and their symmetric difference
+print("Set 1:", set1)
+print("Set 2:", set2)
+print("Symmetric Difference:", symmetric_diff)
